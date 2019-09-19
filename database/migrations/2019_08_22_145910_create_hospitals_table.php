085253b1c81aec9df32c241290e18924fc628cdd
puneet0911 <puneetpremsharma@hotmail.com>
2019-09-19 23:03:34 +0530

    Adding Healthcare laravel API

---
+++ database/migrations/2019_08_22_145910_create_hospitals_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHospitalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hospitals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('image');
+            $table->string('description');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('country');
+            $table->string('number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hospitals');
+    }
+}
